4350015dcf679d6b1a51405919cf72cdf650dc46
Equan Pr <eq@junwatu.com>
2012-03-21 09:38:45 +0700

    modified example with empty application_key.

--- example/simple.php
+++ example/simple.php
@@ -1,10 +1,10 @@
 <?php
 
 // Set the exception handler
-require dirname(__FILE__)."/../amon.php";
+require dirname(__FILE__)."/../Amon.php";
 Amon::config(array('host'=> 'http://127.0.0.1',
          'port' => 2464,
-         'application_key'=>'HpaGahguPrOfsCnMP57FivGlG5fyMRfa0eJO3EUZZRY'));
+         'application_key'=>''));
 Amon::setup_exception_handler();
 
 error_reporting(E_ALL);
